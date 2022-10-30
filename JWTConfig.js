5253296c5f2e6e53ec0205203a1b7386f72bd017
winkinshark <57012776+winkinshark@users.noreply.github.com>
2022-10-30 23:05:28 +0530

    JWT
    JWT Issue

---
+++ JWTConfig.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+const verifiedToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  const verifyToken = verifiedToken
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
